7e22f9c1ccfe1ae2a7bbeb4f801d4c3be2138061
Yulia Bondar <yuliabondar98@gmail.com>
2023-02-04 00:47:46 +0300

    feat: add design of welcome page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// import './WelcomePage.css'
+import BackgroundVideo from '../../components/BackgroundVideo/BackgroundVideo';
+
+import '../../styles/WelcomePage.scss';
 
 const WelcomePage: React.FC = () => {
   const navigate = useNavigate();
   return (
     <section className="welcome-page">
+      <BackgroundVideo />
       <h2 className="page-title">EXPLORE THE WORLD! </h2>
       <p>Find clues and guess where you are in the world. Join 50 million other players worldwide.</p>
       <button onClick={() => navigate('/signup')}>PLAY FREE NOW</button>
