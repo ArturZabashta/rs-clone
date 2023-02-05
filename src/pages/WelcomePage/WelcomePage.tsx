a0f99b575a0bf891c57a331275de1fd91f83a5c3
Yulia Bondar <yuliabondar98@gmail.com>
2023-02-06 02:31:23 +0300

    feat: add design for welcome page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import BackgroundVideo from '../../components/BackgroundVideo/BackgroundVideo';
@@ -7,12 +7,33 @@ import '../../styles/WelcomePage.scss';
 
 const WelcomePage: React.FC = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <section className="welcome-page">
-      <BackgroundVideo />
-      <h2 className="page-title">EXPLORE THE WORLD! </h2>
-      <p>Find clues and guess where you are in the world. Join 50 million other players worldwide.</p>
-      <button onClick={() => navigate('/signup')}>PLAY FREE NOW</button>
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <BackgroundVideo />
+      )}
+      <div className="container">
+        <h2 className="page-title">EXPLORE THE WORLD! </h2>
+        <p className="text-on-video h-mb70">
+          Find clues and guess where you are in the world. Join 50 million other players worldwide.
+        </p>
+        <button onClick={() => navigate('/signup')} className="btn_blue h-m0auto">
+          PLAY FREE NOW
+        </button>
+      </div>
     </section>
   );
 };
