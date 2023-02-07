8c9e3ab85963db3fe23c5c891c044cf0c7eeeeda
Yulia Bondar <yuliabondar98@gmail.com>
2023-02-07 23:40:31 +0300

    feat: update UI and design

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import MyButton from '../../components/MyButton/MyButton';
 
+import '../../styles/SingUpPage.scss';
+
 const SignUpPage: React.FC = () => {
   return (
     <section className="signup-page">
       <h3>Create an account</h3>
-      <span>Email</span>
-      <input></input>
-      <MyButton className="signup_btn" route="/home">
-        sign up
-      </MyButton>
+      <form>
+        <span>Email</span>
+        <input></input>
+        <MyButton className="signup_btn btn_blue" route="/home">
+          sign up
+        </MyButton>
+      </form>
     </section>
   );
 };
