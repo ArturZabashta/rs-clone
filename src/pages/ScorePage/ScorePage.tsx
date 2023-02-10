4c19c2de759198b6b04021dfcb84a66ade002cd2
shish-ko <andrey.shishko@gmail.com>
2023-02-10 17:10:46 +0300

    feat: top score page

--- src/pages/ScorePage/ScorePage.tsx
+++ src/pages/ScorePage/ScorePage.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-// import './SinglePlayer.css'
+import { useAppSelector } from '../../hooks/userHooks';
+
+import ScoreList from './ScoreList';
+
+import '../../styles/scorePage.scss';
 
 const ScorePage: React.FC = () => {
+  const { username, topScores } = useAppSelector((state) => state.ui);
+
   return (
     <section className="score-page">
-      <h2 className="page-title">SinglePlayer Score </h2>
-      <h2 className="page-title">MultiPlayer Score </h2>
+      <h2 className="score-page_title">
+        {' '}
+        <span style={{ textTransform: 'capitalize' }}>{username}&apos;s</span> high scores:{' '}
+      </h2>
+      <ScoreList />
     </section>
   );
 };
