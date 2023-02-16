7c3efb120f8b13b66c4c2806cc6659ef9f8ec5c3
shish-ko <107854857+shish-ko@users.noreply.github.com>
2023-02-17 00:16:09 +0300

    feat: best score block (#18)
    * feat: best score block
    
    * fix: host name at ScorePage
    
    ---------
    
    Co-authored-by: shish-ko <andrey.shishko@gmail.com>

---
+++ src/pages/ScorePage/BestList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { IBestScore } from '../../types/uiInterface';
+
+interface IScoreListProps {
+  scoreList: IBestScore[];
+}
+
+const BestList: React.FC<IScoreListProps> = ({ scoreList }) => {
+  const rest = scoreList.slice(3);
+  return (
+    <div className="best-results">
+      <div className="best-results_top">
+        <div className="top_item">
+          <div className="top_name">{scoreList[1].username}</div>
+          <div className="top2_stand top2_spread">
+            <div className="top_score">{scoreList[1].score}</div>
+          </div>
+        </div>
+        <div className="top_item">
+          <div className="top_name">{scoreList[0].username}</div>
+          <div className="top1_stand top1_spread">
+            <div className="top_score">{scoreList[0].score}</div>
+          </div>
+        </div>
+        <div className="top_item">
+          <div className="top_name">{scoreList[2].username}</div>
+          <div className="top3_stand top3_spread">
+            <div className="top_score">{scoreList[2].score}</div>
+          </div>
+        </div>
+      </div>
+      {rest.map((item, ind) => {
+        return (
+          <div className="best-results_rest" key={ind}>
+            {ind + 4}. Username: {item.username}; score: {item.score}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BestList;
--- src/pages/ScorePage/ScorePage.tsx
+++ src/pages/ScorePage/ScorePage.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useAppSelector } from '../../hooks/userHooks';
+import { IBestScore } from '../../types/uiInterface';
 
+import BestList from './BestList';
 import ScoreList from './ScoreList';
 
 import '../../styles/scorePage.scss';
 
+interface IScoreRes {
+  results: IBestScore[];
+}
+
 const ScorePage: React.FC = () => {
   const { username } = useAppSelector((state) => state.ui);
+  const [bestScores, setBestScores] = useState<IBestScore[]>([]);
+  useEffect(() => {
+    fetch('https://rsclone-server.onrender.com/best_score')
+      .then((res) => res.json())
+      .then((res: IScoreRes) => {
+        setBestScores(res.results);
+      });
+  }, []);
 
   return (
     <section className="score-page">
-      <h2 className="score-page_title">
-        {' '}
-        <span style={{ textTransform: 'capitalize' }}>{username}&apos;s</span> high scores:{' '}
-      </h2>
-      <ScoreList />
+      <div className="score-page_item">
+        <h2 className="score-page_title">Best Scores</h2>
+        {bestScores.length === 0 ? <div className="spinner"></div> : <BestList scoreList={bestScores} />}
+      </div>
+      <div className="score-page_item">
+        <h2 className="score-page_title">
+          {' '}
+          <span style={{ textTransform: 'capitalize' }}>{username}&apos;s</span> high scores:{' '}
+        </h2>
+        <ScoreList />
+      </div>
     </section>
   );
 };
