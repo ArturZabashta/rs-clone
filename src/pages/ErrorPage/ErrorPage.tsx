936eca33601088555e3171632a3eab76825d325e
Yulia Bondar <yuliabondar98@gmail.com>
2023-02-21 02:20:54 +0300

    feat: change 404 page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -6,16 +6,13 @@ import { useAppSelector } from '../../hooks/userHooks';
 const ErrorPage: React.FC = () => {
   const { isLogin } = useAppSelector((state) => state.ui);
   return (
-    <section
-      className="error-page"
-      style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}
-    >
-      <h3 className="error-page_title">Error Page</h3>
-      <h2 className="error-page_text"># 404</h2>
+    <section className="error-page">
+      <h2 className="error-page_text error-title"># 404</h2>
+      <h3 className="error-page_title">Page not found</h3>
+      <p className="f-center text-on-video">Hmm, this page seems to be missing</p>
       <MyButton className={'login_btn'} route={isLogin ? '/home' : '/'}>
         {isLogin ? 'Home' : 'Welcome Page'}
       </MyButton>
-      <p className="f-center text-on-video">Hmm, this page seems to be missing</p>
     </section>
   );
 };
